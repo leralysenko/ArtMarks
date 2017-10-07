9b960853f0bf5258c5ccdef4b6f7d742d92f3495
leralysenko <arellysenko@gmail.com>
2017-10-08 02:49:58 +0300

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,35 @@
+$('.multiple-items').slick({
+    infinite: true,
+  	slidesToShow: 2,
+  	slidesToScroll: 1,
+  	dots: true,
+  	arrows: false,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
